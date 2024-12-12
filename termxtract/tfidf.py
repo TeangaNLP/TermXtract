40e745f0a66322dcff3b91a9ee6e016ba40ea840
Shubhanker Banerjee <shubcode1@gmail.com>
2024-12-12 10:49:53 +0000

    Add Teanga Compatibility

--- termxtract/tfidf.py
+++ termxtract/tfidf.py
@@ -1,8 +1,9 @@
 import re
 from collections import Counter
 import math
-from typing import List, Dict, Optional
+from typing import List, Dict, Optional, Tuple
 from .utils import ATEResults
+
 
 class TFIDFTermExtractor:
     """TF-IDF based term extraction with n-gram support and a threshold option."""
@@ -11,7 +12,37 @@ class TFIDFTermExtractor:
         self.threshold = threshold
         self.n = n
 
-    def generate_ngrams(self, words: List[str]) -> List[str]:
+    def generate_ngrams_teanga(self, words_with_offsets: List[Tuple[int, int, str]]) -> List[Tuple[str, Tuple[int, int]]]:
+        """
+        Generate n-grams with offsets for a Teanga corpus.
+
+        Args:
+            words_with_offsets (List[Tuple[int, int, str]]): List of (start, end, text) tuples for words.
+
+        Returns:
+            List[Tuple[str, Tuple[int, int]]]: List of n-grams with their start and end offsets.
+        """
+        ngrams = []
+        words = [text for _, _, text in words_with_offsets]
+        for i in range(len(words)):
+            for j in range(1, self.n + 1):
+                if i + j <= len(words):
+                    ngram = " ".join(words[i:i + j])
+                    start_offset = words_with_offsets[i][0]  # Start offset of the first word in the n-gram
+                    end_offset = words_with_offsets[i + j - 1][1]  # End offset of the last word in the n-gram
+                    ngrams.append((ngram, (start_offset, end_offset)))
+        return ngrams
+
+    def generate_ngrams_strings(self, words: List[str]) -> List[str]:
+        """
+        Generate n-grams for a plain list of strings.
+
+        Args:
+            words (List[str]): List of words in the document.
+
+        Returns:
+            List[str]: List of n-grams.
+        """
         ngrams = []
         for i in range(len(words)):
             for j in range(1, self.n + 1):
@@ -40,7 +71,7 @@ class TFIDFTermExtractor:
         for doc_id in corpus.doc_ids:
             doc = corpus.doc_by_id(doc_id)
             words_with_offsets = [(start, end, doc.text[start:end]) for start, end in doc.words]
-            ngrams_with_offsets = self.generate_ngrams(words_with_offsets)
+            ngrams_with_offsets = self.generate_ngrams_teanga(words_with_offsets)
             ngrams_by_doc[doc_id] = ngrams_with_offsets
 
         corpus_ngrams = [[ngram for ngram, _ in ngrams_with_offsets] for ngrams_with_offsets in ngrams_by_doc.values()]
@@ -66,7 +97,7 @@ class TFIDFTermExtractor:
 
     def extract_terms_strings(self, corpus: List[str]) -> ATEResults:
         tokenized_corpus = [re.findall(r'\b\w+\b', doc.lower()) for doc in corpus]
-        corpus_ngrams = [self.generate_ngrams(doc) for doc in tokenized_corpus]
+        corpus_ngrams = [self.generate_ngrams_strings(doc) for doc in tokenized_corpus]
         idf = self.compute_idf(corpus_ngrams)
 
         terms_by_doc = []
@@ -75,7 +106,7 @@ class TFIDFTermExtractor:
             doc_id = f"doc_{idx}"
             processed_corpus.append({"doc_id": doc_id, "text": doc_text})
 
-            doc_ngrams = self.generate_ngrams(tokenized_corpus[idx])
+            doc_ngrams = self.generate_ngrams_strings(tokenized_corpus[idx])
             tf = self.compute_tf(doc_ngrams)
             tfidf = {ngram: tf[ngram] * idf[ngram] for ngram in tf}
 
